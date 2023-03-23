2501c6392452d930e01ccf0282471a34a5c6889b
Diana Johnson <dianajohnson13@gmail.com>
2023-03-23 10:19:29 -0700

    Basic EntryHeader html

--- src/components/EntryHeader.js
+++ src/components/EntryHeader.js
@@ -1,10 +1,27 @@
 
-function EntryHeader(props) {
-    // word
-    // pronounciation
-    // sound clip
-    console.log(props)
-    return null;
+function EntryHeader({
+    word,
+    phonetic // {audio, text} can be missing; TO-DO: handle better
+}) {
+    const playAudio = () => {
+        const audioClip = new Audio(phonetic.audio);
+        audioClip.play();
+    }
+
+    return (
+        <div>
+            <div>
+                <h1>{word}</h1>
+                {phonetic && phonetic.text ? (<p>{phonetic.text}</p>) : null}
+            </div>
+            <button
+                disabled={!phonetic || !phonetic.audio}
+                onClick={playAudio}
+            >
+                Play
+            </button>
+        </div>
+    );
 }
 
 export default EntryHeader;
