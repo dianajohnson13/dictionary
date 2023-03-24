c823f268a69a16c49603747f4c46e775d01c3d03
Diana Johnson <dianajohnson13@gmail.com>
2023-03-24 13:36:01 -0700

    Break out component for synonyms and antonyms

--- src/components/Meaning.js
+++ src/components/Meaning.js
@@ -1,3 +1,4 @@
+import RelatedWordList from "./RelatedWordList";
 
 // partOfSpeech: string
 // definitions: [{definition: string}]
@@ -19,38 +20,18 @@ function Meaning({
                    return <li key={key}>{item.definition}</li>
                 })}
             </ul>
-
-            {/* TO-DO: turn lists below into components */}
-            {synonyms.length > 0 ? (
-                <div>
-                <h3>Synonyms</h3>
-                <ul>
-                    {synonyms.map((syn, idx) => {
-                        return (
-                            <li key={idx}>
-                                <a>{syn}</a>
-                                {idx < synonyms.length -1 ? ", " : null}
-                            </li>
-                        );
-                    })}
-                </ul>
-                </div>
-            ) : null}
-            {antonyms.length > 0 ? (
-                <div>
-                    <h3>Antonyms</h3>
-                    <ul>
-                        {antonyms.map((ant, idx) => {
-                            return (
-                                <li key={idx}>
-                                    <a>{ant}</a>
-                                    {idx < synonyms.length -1 ? ", " : null}
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            ) : null}
+            {synonyms.length > 0 && (
+                <RelatedWordList
+                    label="Synonyms"
+                    list={synonyms}
+                />
+            )}
+            {antonyms.length > 0 && (
+                <RelatedWordList
+                    label="Antonyms"
+                    list={antonyms}
+                />
+            )}
         </div>
     );
 }
---
+++ src/components/RelatedWordList.js
@@ -0,0 +1,25 @@
+import "../styles/RelatedWordList.css";
+
+function RelatedWordList({
+    label,
+    list
+}) {
+    return (
+        <div className="related-words-container">
+            <h3>{label}</h3>
+            <ul>
+                {list.map((word, idx) => (
+                        <li key={idx}>
+                            <a>{word}</a>
+                            {/* unless it's the last word in the list, add a comma */}
+                            {idx < list.length -1 ? ", " : null}
+                        </li>
+                    )
+                )}
+            </ul>
+        </div>
+    );
+}
+
+
+export default RelatedWordList;
